e587ef9dfb1f0245201e54bbfee0152a4ff5f61c
Ethan Burkinshaw <ethanburkinshaw@icloud.com>
2020-08-26 16:52:41 +0100

    did stuff

--- Connect4/script.js
+++ Connect4/script.js
@@ -1,3 +1,22 @@
+const canvas = document.getElementById("game");
+canvas.width = canvas.height = 600;
+var ctx = canvas.getContext('2d');
+
+const yellowCounter = new Image
+yellowCounter.onload = function(){ drawGrid() };
+yellowCounter.src = "counterYellow.svg"
+
+const redCounter = new Image
+redCounter.onload = function(){ drawGrid() };
+redCounter.src = "counterRed.svg"
+
+const whiteCounter = new Image
+whiteCounter.onload = function(){ drawGrid() };
+whiteCounter.src = "counterWhite.svg"
+
+
+
+
 function input(e){
     if(isNaN(e) == false){
         e = parseInt(e);
@@ -16,24 +35,25 @@ enter.addEventListener("keyup", function(event) {
     }
 });
 
+let playing = true;
 let winType = 0;
-let player = 0
+let player = 0;
 function makeMove(col){
     if(is_valid_location(board,col)){
         let row = getNextOpenRow(board,col);
         dropPiece(board,row,col,player +1);
+        drawGrid()
         if(winning_move(board,player +1)){
             console.log("WINNNER")
             console.log(winType)
+            playing = false;
         }
+        player++
+        player = player % 2;
     }else{
         console.log("invalid location")
     }
     printBoard()
-
-
-    player++
-    player = player % 2;
 }
 
 let rowCount = 6
@@ -57,12 +77,14 @@ function getNextOpenRow(board,col){
     }
 }
 
+let winner;
 function winning_move(board,piece){
     //Check horizontal win
     for(let c=0;c<columnCount-3;c++){
         for(let r=0;r<rowCount;r++){
             if(board[r][c] == piece && board[r][c+1] == piece && board[r][c+2] == piece && board[r][c+3] == piece){
                 winType=1
+                winner = board[r][c]
                 return true;
             }
         }
@@ -73,6 +95,7 @@ function winning_move(board,piece){
         for(let r=0;r<rowCount-3;r++){
             if(board[r][c] == piece && board[r+1][c] == piece && board[r+2][c] == piece && board[r+3][c] == piece){
                 winType=2
+                winner = board[r][c]
                 return true;
             }
         }
@@ -83,6 +106,7 @@ function winning_move(board,piece){
         for(let r=0;r<rowCount-3;r++){
             if(board[r][c] == piece && board[r+1][c+1] == piece && board[r+2][c+2] == piece && board[r+3][c+3] == piece){
                 winType=3
+                winner = board[r][c]
                 return true;
             }
         }
@@ -93,6 +117,7 @@ function winning_move(board,piece){
         for(let r=3;r<rowCount;r++){
             if(board[r][c] == piece && board[r-1][c+1] == piece && board[r-2][c+2] == piece && board[r-3][c+3] == piece){
                 winType=4
+                winner = board[r][c]
                 return true;
             }
         }
@@ -118,5 +143,91 @@ function printBoard(){
     for(let i=5; i>=0;i--){
         console.log(i)
         console.log(board[i].toString())
+    }
+}
+
+
+canvas.addEventListener('mousedown', function(e) {
+    getCursorPosition(canvas, e)
+})
+
+function getCursorPosition(canvas,e){
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    function getLocation(co){
+        for(let i=7;i>=0;i--){
+            if(co>= (canvas.width/7) *i){
+                return i
+            }
+        }
+        return 0;
+    }
+
+    xSet = getLocation(x);
+    console.log(xSet)
+    input(xSet)
+}
+
+initialize();
+
+function initialize() {
+    // Register an event listener to call the resizeCanvas() function 
+    // each time the window is resized.
+    window.addEventListener('resize', resizeCanvas, false);
+    // Draw canvas border for the first time.
+    resizeCanvas();
+}
+
+// Runs each time the DOM window resize event fires.
+// Resets the canvas dimensions to match window,
+// then draws the new borders accordingly.
+function resizeCanvas() {
+    if(window.innerWidth > window.innerHeight){
+        windowSize = window.innerHeight -200
+    }else{
+        windowSize = window.innerWidth -100
+    }
+    canvas.width = windowSize;
+    canvas.height = windowSize;
+    drawGrid()
+}
+
+function drawGrid(){
+    if(playing == false){
+        ctx.fillStyle = "#ffffff"
+        if(winner == 1){
+            ctx.font = "30px Arial"
+            ctx.fillText("Yellow has won the game",1,50)
+        }else{
+            ctx.font = "30px Arial"
+            ctx.fillText("Red has won the game",1,50)
+        }
+    }else{
+        let boardY = canvas.width/6 *5;
+        if(player==1){
+            ctx.fillStyle = "#ffec5b"
+            ctx.fillRect(0,0,canvas.width,boardY)
+        }else{
+            ctx.fillStyle = "#ff5050"
+            ctx.fillRect(0,0,canvas.width,boardY)
+        }
+        ctx.fillStyle = "#4551f7"
+        ctx.fillRect(0,canvas.width -boardY,canvas.width,boardY)
+        for(let row=0;row<rowCount;row++){
+            for(let col=0;col<columnCount;col++){
+                let size = canvas.width/13;
+                let r= rowCount-row-1
+                let x=(canvas.width/columnCount *col) -size/2 +size
+                let y=((boardY/rowCount *r) -size/2 +size) + canvas.height - boardY
+                if(board[row][col] == 0){
+                    ctx.drawImage(whiteCounter,x,y,size,size)
+                }else if(board[row][col] == parseInt(1)){
+                    ctx.drawImage(yellowCounter,x,y,size,size)
+                }else{
+                    ctx.drawImage(redCounter,x,y,size,size)
+                }
+            }
+        }
     }
 }
